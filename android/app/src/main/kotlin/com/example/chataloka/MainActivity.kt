211f978dec0eaaa88e09dbf5da0eeb3831706567
rakafebriansy <kerenr445@gmail.com>
2025-02-24 07:38:11 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/chataloka/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chataloka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chataloka/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chataloka',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
