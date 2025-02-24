218383515e49245551efff03de970b60a1530f7c
rakafebriansy <kerenr445@gmail.com>
2025-02-24 08:50:50 +0700

    feat: adding themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,40 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:chataloka/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.savedThemeMode});
+
+  final AdaptiveThemeMode? savedThemeMode;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chataloka',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return AdaptiveTheme(
+      light: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.deepPurple,
       ),
-      home: const HomeScreen(),
+      dark: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.deepPurple,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder:
+          (theme, darkTheme) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Chataloka',
+            theme: theme,
+            darkTheme: darkTheme,
+            home: const HomeScreen(),
+          ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,11 +9,61 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isDarkMode = false;
+
+  // Method: get the saved theme mode
+  void getThemeMode() async {
+    final savedThemeMode = await AdaptiveTheme.getThemeMode();
+    if (savedThemeMode == AdaptiveThemeMode.dark) {
+      setState(() {
+        isDarkMode = true;
+      });
+    } else {
+      setState(() {
+        isDarkMode = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getThemeMode();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Home'),
+      body: Center(
+        child: Card(
+          child: SwitchListTile(
+            title: const Text('Change Theme'),
+            secondary: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isDarkMode ? Colors.white : Colors.black
+              ),
+              child: Icon(
+                isDarkMode ? Icons.nightlight_round : Icons.wb_sunny_rounded,
+                color: isDarkMode ? Colors.black : Colors.white,
+              )
+            ),
+            value: isDarkMode,
+            onChanged: (value) {
+              setState(() {
+                isDarkMode = value;
+              });
+
+              if (value) {
+                AdaptiveTheme.of(context).setDark();
+              } else {
+                AdaptiveTheme.of(context).setLight();
+              }
+            },
+          ),
+        ),
       ),
     );
   }
