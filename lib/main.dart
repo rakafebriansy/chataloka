b1e04dbc3bc05e3e599000c5392f6b19ebb8c40a
rakafebriansy <kerenr445@gmail.com>
2025-03-10 16:03:19 +0700

    feat: connect intp firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:chataloka/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
   runApp(MyApp(savedThemeMode: savedThemeMode));
 }
